79e987bb74b20e4128595493d0f1223233dc16fd
Jon Lalabalavu <Jon@Jons-MacBook-Pro.local>
2025-10-02 12:26:11 +0100

    Remove large files and update .gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/Jon/0 A VIBE CODER PROJECTS MACBOOK PRO/proposalflow/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
